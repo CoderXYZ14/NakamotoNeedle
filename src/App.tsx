011a2212e73695023cb9cdfa4044aba3eccee00a
CoderXYZ14 <s1404islam@gmail.com>
2024-09-20 05:25:53 +0530

    Cached value displayed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { AmountInput, ResultRow } from "./components";
+import { sortBy } from "lodash";
 
+type CachedResult = {
+  provider: string;
+  btc: string;
+};
 function App() {
   const [amount, setAmount] = useState("100");
+  const [cachedResults, setCachedResult] = useState<CachedResult[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    axios.get("http://localhost:3000/cached").then((res) => {
+      setCachedResult(res.data);
+      setIsLoading(false);
+    });
+  }, []);
   return (
     <main className="max-w-2xl mx-auto px-4 py-8">
       <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">
@@ -15,9 +29,20 @@ function App() {
         />
       </div>
       <div className="mt-6">
-        <ResultRow />
-        <ResultRow />
-        <ResultRow />
+        {isLoading && (
+          <>
+            <ResultRow loading={true} />
+            <ResultRow loading={true} />
+            <ResultRow loading={true} />
+            <ResultRow loading={true} />
+          </>
+        )}
+        {!isLoading &&
+          sortBy(cachedResults, "btc")
+            .reverse()
+            .map((result: CachedResult) => (
+              <ResultRow providerName={result.provider} btc={result.btc} />
+            ))}
       </div>
     </main>
   );
--- src/components/ResultRow.tsx
+++ src/components/ResultRow.tsx
@@ -1,16 +1,25 @@
 type ResultRowProps = {
   loading?: boolean;
+  providerName?: string;
+  btc?: string;
 };
+
 const ResultRow = (props: ResultRowProps) => {
   return (
-    <div className="relative border min-h-12 border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2 overflow-hidden">
-      <div className="flex gap-4">
-        <div>Logo</div>
-        <div className="grow">Provider Name</div>
-        <div className="flex gap-2">
-          <span className="text-xl text-purple-200/80">0.01</span>
-          <span className="text-xl text-purple-300/50">BTC</span>
-        </div>
+    <div className="relative border min-h-16 border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2 overflow-hidden">
+      <div className="flex gap-4 items-center">
+        {props.providerName && <div>Logo</div>}
+        <div className="grow">{props.providerName || ""}</div>
+        {props.btc !== undefined && (
+          <div className="flex gap-2">
+            <span className="text-xl text-purple-200/80">
+              {new Intl.NumberFormat("en-IN", {
+                minimumFractionDigits: 8,
+              }).format(parseFloat(props.btc))}
+            </span>
+            <span className="text-xl text-purple-300/50">BTC</span>
+          </div>
+        )}
       </div>
       {props.loading && (
         <div className="inset-0 absolute rounded-lg bg-gradient-to-r from-transparent via-blue-800/50 to-transparent skeleton-animation"></div>
